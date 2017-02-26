33dff438dbe418c50035568f730590f14450fb77
yacut <yacut@protonmail.com>
2017-02-26 20:36:46 +0100

    init commit

---
+++ spec/test-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestRunnerView from '../lib/test-runner-view';
+
+describe('TestRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
