13ea9d4d5746aa13bfebccca2e35527306e02247
yacut <yacut@protonmail.com>
2017-03-16 23:21:03 +0100

    add tests

---
+++ spec/decorate-manager-spec.js
@@ -0,0 +1,8 @@
+'use babel';
+
+/* @flow */
+describe('output-view', () => {
+  it('should write more tests', () => {
+    expect('i should write more tests').toNotEqual('this component has a lot of tests :)');
+  });
+});
