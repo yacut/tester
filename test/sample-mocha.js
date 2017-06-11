4bd1c628e4f2a964733b8ac3e771c3722b98f87e
yacut <roman.rogozhnikov@gmail.com>
2017-06-11 19:43:35 +0200

    refactor

--- test/sample-mocha.js
+++ test/sample-mocha.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 
 describe('Array', () => {
   it('should return -1 when the value is not present', () => {
-    assert.equal(-1, [1, 2, 3].indexOf(4));
+    assert.equal(-1, [1, 2, 3].indexOf(2));
   });
 });
