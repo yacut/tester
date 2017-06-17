8b05685f5c824e9ef543a432f52afba6e87db4e1
yacut <roman.rogozhnikov@gmail.com>
2017-06-17 15:37:38 +0200

    fix commands test

--- spec/commands-spec.js
+++ spec/commands-spec.js
@@ -4,7 +4,7 @@
 
 import Path from 'path';
 import Commands from '../lib/commands';
-import { sleep } from './common';
+import { asyncTest } from './common';
 
 let commands;
 
@@ -16,44 +16,49 @@ describe('Tester Commands', () => {
     commands = new Commands();
   });
 
-  it('should properly notifies its listeners of command execution', async () => {
+  it('should properly notifies its listeners of command execution', asyncTest(async (done) => {
     let testCalled = 0;
     let testProjectCalled = 0;
     let toggleOutputCalled = 0;
     let toggleResultsCalled = 0;
 
     commands.onShouldTest(() => (testCalled += 1));
-    commands.onShouldTest(() => (testProjectCalled += 1));
+    commands.onShouldTestProject(() => (testProjectCalled += 1));
     commands.onShouldToggleTesterOutput(() => (toggleOutputCalled += 1));
     commands.onShouldToggleTesterResultView(() => (toggleResultsCalled += 1));
 
     await atom.workspace.open(Path.join(__dirname, 'fixtures', 'test.txt'));
     const textEditor = atom.views.getView(atom.workspace.getActiveTextEditor());
-    // Open events need a tick to process.
-    await sleep(0);
+    const workspaceElement = atom.views.getView(atom.workspace);
+
     expect(testCalled).toBe(0);
     expect(testProjectCalled).toBe(0);
     expect(toggleOutputCalled).toBe(0);
     expect(toggleResultsCalled).toBe(0);
+
     atom.commands.dispatch(textEditor, 'tester:test');
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(0);
     expect(toggleOutputCalled).toBe(0);
     expect(toggleResultsCalled).toBe(0);
-    atom.commands.dispatch(textEditor, 'tester:test-project');
+
+    atom.commands.dispatch(workspaceElement, 'tester:test-project');
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(1);
     expect(toggleOutputCalled).toBe(0);
     expect(toggleResultsCalled).toBe(0);
-    atom.commands.dispatch(textEditor, 'tester:toggle-tester-output');
+
+    atom.commands.dispatch(workspaceElement, 'tester:toggle-tester-output');
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(1);
     expect(toggleOutputCalled).toBe(1);
     expect(toggleResultsCalled).toBe(0);
-    atom.commands.dispatch(textEditor, 'tester:toggle-tester-result');
+
+    atom.commands.dispatch(workspaceElement, 'tester:toggle-tester-result');
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(1);
     expect(toggleOutputCalled).toBe(1);
     expect(toggleResultsCalled).toBe(1);
-  });
+    done();
+  }));
 });
