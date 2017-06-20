f579cb0f02999aeb6a7b7e6950466648561df23f
yacut <roman.rogozhnikov@gmail.com>
2017-06-20 20:13:57 +0200

    more tests

--- spec/commands-spec.js
+++ spec/commands-spec.js
@@ -17,11 +17,19 @@ describe('Tester Commands', () => {
   });
 
   it('should properly notifies its listeners of command execution', asyncTest(async (done) => {
+    let clearCalled = 0;
+    let goToNextTestCalled = 0;
+    let goToPreviousTestCalled = 0;
+    let stopCalled = 0;
     let testCalled = 0;
     let testProjectCalled = 0;
     let toggleOutputCalled = 0;
     let toggleResultsCalled = 0;
 
+    commands.onShouldClear(() => (clearCalled += 1));
+    commands.onShouldGoToNextTest(() => (goToNextTestCalled += 1));
+    commands.onShouldGoToPreviousTest(() => (goToPreviousTestCalled += 1));
+    commands.onShouldStop(() => (stopCalled += 1));
     commands.onShouldTest(() => (testCalled += 1));
     commands.onShouldTestProject(() => (testProjectCalled += 1));
     commands.onShouldToggleTesterOutput(() => (toggleOutputCalled += 1));
@@ -31,30 +39,90 @@ describe('Tester Commands', () => {
     const textEditor = atom.views.getView(atom.workspace.getActiveTextEditor());
     const workspaceElement = atom.views.getView(atom.workspace);
 
+    expect(clearCalled).toBe(0);
+    expect(goToNextTestCalled).toBe(0);
+    expect(goToPreviousTestCalled).toBe(0);
+    expect(stopCalled).toBe(0);
+    expect(testCalled).toBe(0);
+    expect(testProjectCalled).toBe(0);
+    expect(toggleOutputCalled).toBe(0);
+    expect(toggleResultsCalled).toBe(0);
+
+    atom.commands.dispatch(workspaceElement, 'tester:clear');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(0);
+    expect(goToPreviousTestCalled).toBe(0);
+    expect(stopCalled).toBe(0);
+    expect(testCalled).toBe(0);
+    expect(testProjectCalled).toBe(0);
+    expect(toggleOutputCalled).toBe(0);
+    expect(toggleResultsCalled).toBe(0);
+
+    atom.commands.dispatch(workspaceElement, 'tester:go-to-next-test');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(0);
+    expect(stopCalled).toBe(0);
+    expect(testCalled).toBe(0);
+    expect(testProjectCalled).toBe(0);
+    expect(toggleOutputCalled).toBe(0);
+    expect(toggleResultsCalled).toBe(0);
+
+    atom.commands.dispatch(workspaceElement, 'tester:go-to-previous-test');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(1);
+    expect(stopCalled).toBe(0);
+    expect(testCalled).toBe(0);
+    expect(testProjectCalled).toBe(0);
+    expect(toggleOutputCalled).toBe(0);
+    expect(toggleResultsCalled).toBe(0);
+
+    atom.commands.dispatch(workspaceElement, 'tester:stop');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(1);
+    expect(stopCalled).toBe(1);
     expect(testCalled).toBe(0);
     expect(testProjectCalled).toBe(0);
     expect(toggleOutputCalled).toBe(0);
     expect(toggleResultsCalled).toBe(0);
 
     atom.commands.dispatch(textEditor, 'tester:test');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(1);
+    expect(stopCalled).toBe(1);
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(0);
     expect(toggleOutputCalled).toBe(0);
     expect(toggleResultsCalled).toBe(0);
 
     atom.commands.dispatch(workspaceElement, 'tester:test-project');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(1);
+    expect(stopCalled).toBe(1);
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(1);
     expect(toggleOutputCalled).toBe(0);
     expect(toggleResultsCalled).toBe(0);
 
     atom.commands.dispatch(workspaceElement, 'tester:toggle-tester-output');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(1);
+    expect(stopCalled).toBe(1);
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(1);
     expect(toggleOutputCalled).toBe(1);
     expect(toggleResultsCalled).toBe(0);
 
     atom.commands.dispatch(workspaceElement, 'tester:toggle-tester-result');
+    expect(clearCalled).toBe(1);
+    expect(goToNextTestCalled).toBe(1);
+    expect(goToPreviousTestCalled).toBe(1);
+    expect(stopCalled).toBe(1);
     expect(testCalled).toBe(1);
     expect(testProjectCalled).toBe(1);
     expect(toggleOutputCalled).toBe(1);
